bc109511335d690abc28a379350fb200cc32b89f
isd9194 <sachindangi942@gmail.com>
2025-12-29 19:21:52 +0530

    change domain name

--- Frentend/src/components/MyForms/Configs.js
+++ Frentend/src/components/MyForms/Configs.js
@@ -1,3 +1,4 @@
-export const  DOMAIN = "http://localhost:7000/"
+export const  DOMAIN = "https://e-com-web-1-03lj.onrender.com"
+// "http://localhost:7000/"
 // export const  DOMAIN = "http://13.203.103.67:7000/"
 // http://sachindangi.s3-website.eu-north-1.amazonaws.com/
